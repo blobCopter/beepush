352c1acf066a68759dff5cf18adcb6646d22c91d
Julien <julien.darsel@gmail.com>
2013-11-09 17:13:29 +0100

    setting new event model

--- api/models/Event.js
+++ api/models/Event.js
@@ -9,11 +9,27 @@
 module.exports = {
 
   attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
+  		Title: {
+			type: 'string',
+		 	required: true,
+		 	maxLength: 64
+		},
+		Type: {
+			type: 'integer',
+			defaultsTo: 0 //0 ==> MANUAL
+		},
+		Description: {
+			type: 'string',
+			maxLength: 256
+		},
+		user_id: {
+			type: 'integer',
+			required: true
+		},
+		is_public: {
+			type: 'boolean',
+			defaultsTo: true
+		}
   }
 
 };
