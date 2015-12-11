07bededb832576e9d408b73bc55280b8bfb6b394
Julien Darsel <julien.darsel@gmail.com>
2015-12-11 23:26:04 +0100

    initialize angular struct & karma test with gulp

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('beepush', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
